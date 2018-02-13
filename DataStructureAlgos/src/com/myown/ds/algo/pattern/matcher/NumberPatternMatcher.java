6168d3a7f783e07002e76c34ee4adda8806b5377
Mamta Garg <mamta.garg915@gmail.com>
2018-02-14 04:47:54 +0530

    NumberPatternMatcher logic changed

--- DataStructureAlgos/src/com/myown/ds/algo/pattern/matcher/NumberPatternMatcher.java
+++ DataStructureAlgos/src/com/myown/ds/algo/pattern/matcher/NumberPatternMatcher.java
@@ -20,27 +20,47 @@ public class NumberPatternMatcher {
 		System.out.println("Enter number of digits : ");
 		int num = scanner.nextInt();
 		int[] arr = new int[num];
-		List<Integer> list = new ArrayList<>();
 		System.out.println("Enter numbers : ");
 		for (int index = 0; index < num; index++) {
 			arr[index] = scanner.nextInt();
 		}
-		for (int index = 0; index < num; index++) {
-			int n = arr[index];
-			int digit, count = 0;
-			while (n > 0) {
-				digit = n % 10; // get the digit
-				if (digit == 1 || digit == 2 || digit == 3)
-					count++;
-				n = n / 10;
-			}
-			if (count >= 3) {
-				list.add(arr[index]);
-			}
-		}
-		Collections.sort(list);
-		System.out.println("Output containing 1, 2 and 3 : ");
-		list.forEach(value->System.out.print(value + " "));
+		String result = findQualifiedNumbers(arr);
+		System.out.println("Output containing 1, 2 and 3 : " + result);
 	}
+	
+	/**
+	 * Find qualified numbers.
+	 *
+	 * @param numberArray the number array
+	 * @return the string
+	 */
+	static String findQualifiedNumbers(int[] numberArray) {
+        List<Integer> eligibleNumbers = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+        for(int index=0; index<numberArray.length; index++) {
+
+            //Converting the number to string, with this we'll be able to call the contains method directly.
+            String numberString = String.valueOf(numberArray[index]);
+            // Main condition
+            if (numberString.contains("1") && numberString.contains("2") && numberString.contains("3") ) {
+                eligibleNumbers.add(numberArray[index]);
+            }
+        }
+        // Sorting the result as the per the requirement.
+        Collections.sort(eligibleNumbers);
+
+        // Append a , as required
+        for(int item:eligibleNumbers) {
+            builder.append(item).append(",");
+        }
+        //removing the additional, at the end.
+        if(builder.length()>0) {
+            builder.setLength(builder.length()-1);
+            return builder.toString();
+        } else {
+            //edge case
+            return "-1";
+        }
+    }
 
 }
